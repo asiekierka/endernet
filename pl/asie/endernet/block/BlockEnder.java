b4bf80748d1b7d70dc3f5bb2290ba04e0b0652c1
asiekierka <asiekierka@gmail.com>
2013-12-14 18:36:29 +0100

    drop items for transmitter

--- pl/asie/endernet/block/BlockEnder.java
+++ pl/asie/endernet/block/BlockEnder.java
@@ -1,9 +1,15 @@
 package pl.asie.endernet.block;
 
+import java.util.Random;
+
 import pl.asie.endernet.EnderNet;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
@@ -42,6 +48,42 @@ public class BlockEnder extends BlockContainer {
     @Override
 	public void breakBlock(World world, int x, int y, int z, int id, int meta) {
     	this.onBlockDestroyed(world, x, y, z, meta);
+    	this.dropItems(world, x, y, z);
     	super.breakBlock(world, x, y, z, id, meta);
 	}
+    
+    public static void dropItems(World world, int x, int y, int z) {
+    	Random rand = new Random();
+    	TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
+    	if (tileEntity == null || !(tileEntity instanceof IInventory)) {
+    		return;
+    	}
+    	IInventory inventory = (IInventory) tileEntity;
+
+    	for (int i = 0; i < inventory.getSizeInventory(); i++) {
+    		ItemStack item = inventory.getStackInSlot(i);
+
+    		if (item != null && item.stackSize > 0) {
+    			inventory.setInventorySlotContents(i, null);
+    			float rx = rand.nextFloat() * 0.8F + 0.1F;
+    			float ry = rand.nextFloat() * 0.8F + 0.1F;
+    			float rz = rand.nextFloat() * 0.8F + 0.1F;
+
+    			EntityItem entityItem = new EntityItem(world,
+    					x + rx, y + ry, z + rz,
+    					new ItemStack(item.itemID, item.stackSize, item.getItemDamage()));
+
+    			if (item.hasTagCompound()) {
+    				entityItem.getEntityItem().setTagCompound((NBTTagCompound)item.getTagCompound().copy());
+    			}
+
+    			float factor = 0.05F;
+    			entityItem.motionX = rand.nextGaussian() * factor;
+    			entityItem.motionY = rand.nextGaussian() * factor + 0.2F;
+    			entityItem.motionZ = rand.nextGaussian() * factor;
+    			world.spawnEntityInWorld(entityItem);
+    			item.stackSize = 0;
+    		}
+    	}
+}
 }
