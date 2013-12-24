313b3e3b7069f2e0e28e3690471f60954af9dd29
asiekierka <asiekierka@gmail.com>
2013-12-24 13:06:14 +0100

    messageFormat

--- pl/asie/endernet/chat/ChatHandler.java
+++ pl/asie/endernet/chat/ChatHandler.java
@@ -1,5 +1,8 @@
 package pl.asie.endernet.chat;
 
+import java.util.Date;
+import java.util.HashMap;
+
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
@@ -7,6 +10,7 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ChatMessageComponent;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.ConfigCategory;
 import net.minecraftforge.common.Configuration;
 import net.minecraftforge.common.Property;
 import net.minecraftforge.event.ForgeSubscribe;
@@ -16,9 +20,10 @@ import pl.asie.endernet.block.TileEntityEnderChatBox;
 import pl.asie.endernet.lib.EntityCoord;
 
 public class ChatHandler {
+	private HashMap<String, String> actions = new HashMap<String, String>();
 	public boolean enableChatFeatures, enableShout, enableGreentext;
 	public int HEARING_DISTANCE, SPEAKING_DISTANCE, CHAT_RADIUS;
-	public String colorAction;
+	public String colorAction, messageFormat;
 	
 	public ChatHandler(Configuration config) {
 		HEARING_DISTANCE = config.get("chat", "chatboxHearingDistance", 12).getInt();
@@ -29,14 +34,30 @@ public class ChatHandler {
 		config.get("chat", "enableGreentext", false).comment = ">implying anyone will ever turn this on";
 		enableGreentext = config.get("chat", "enableGreentext", false).getBoolean(false);
 		colorAction = config.get("chat", "colorAction", "5").getString();
+		messageFormat = config.get("chat", "messageFormat", "<%u> %m").getString();
+		ConfigCategory chatActions = config.getCategory("chatactions");
+		chatActions.setComment("Define chat actions here. S:commandName=action. Only works when enableChatFeatures is true. If empty, will add in the default ones.");
+		if(chatActions.isEmpty()) {
+			config.get("chatactions", "hug", "hugs PLAYER!");
+			config.get("chatactions", "slap", "slaps PLAYER around a bit with a large trout");
+		}
+		for(String key: chatActions.keySet()) {
+			actions.put(key, chatActions.get(key).getString());
+		}
 	}
 	
 	public void registerCommands(FMLServerStartingEvent event) {
 		if(enableChatFeatures) {
 			event.registerServerCommand(new CommandMe());
-			event.registerServerCommand(new CommandAction("hug", "hugs PLAYER!"));
-			event.registerServerCommand(new CommandAction("slap", "slaps PLAYER around a bit with a large trout"));
+			for(String key: actions.keySet()) {
+				event.registerServerCommand(new CommandAction(key, actions.get(key)));
+			}
 		}
+	}
+	
+	private static String pad(int t) {
+		if(t < 10) return "0"+t;
+		else return ""+t;
 	}
 	
 	@ForgeSubscribe
@@ -59,11 +80,20 @@ public class ChatHandler {
 				message = EnumChatFormatting.GREEN + message;
 			}
 			
+			Date now = new Date();
+			String formattedMessage = EnumChatFormatting.RESET + messageFormat.replaceAll("&", "\u00a7")
+					.replaceAll("%u", username)
+					.replaceAll("%m", message)
+					.replaceAll("%w", event.player.worldObj.provider.getDimensionName())
+					.replaceAll("%H", pad(now.getHours()))
+					.replaceAll("%M", pad(now.getMinutes()))
+					.replaceAll("%S", pad(now.getSeconds()));
+			
 			if(event.message.startsWith("!") && enableShout) {
-				chat.addText(EnumChatFormatting.YELLOW + "[Shout] " + EnumChatFormatting.RESET + "<" + username + "> " + message);
+				chat.addText(EnumChatFormatting.YELLOW + "[Shout] " + formattedMessage);
 				disableRadius = true;
 			} else {
-				chat.addText("<" + username + "> " + message);
+				chat.addText(formattedMessage);
 			}
 		}
 		
